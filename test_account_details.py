ac489b522b6a7bc2ca214a32f4dc25b23ab42ab4
shreyamchakrasali-hash <shreyamchakrasali@gmail.com>
2026-01-08 17:51:13 +0530

    Update account details formatting in tests

--- test_account_details.py
+++ test_account_details.py
@@ -1,9 +1,9 @@
 from account_details import account_details
 def account_details():
     output=(
-        "Account number: 123456789\n"
-        "Account holder: vaishnavi\n"
-        "Account type: savings\n"
-        "Balance: 55000"
+        "acc_number:123456789\n"
+        "holder_name:vaishnavi\n"
+        "acc_type:savings\n"
+        "balance:55000"
     )
-    assert account_details("123456789","vaishnavi" "savings", 55000) == output
+    assert account_details("123456789","vaishnavi","savings",55000)==output
